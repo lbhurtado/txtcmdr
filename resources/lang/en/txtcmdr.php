2514f99e820a517a537529835ce74534bc7ae96c
Lester Hurtado <lester@hurtado.ph>
2019-01-29 14:05:31 +0800

    add notification template to CommanderGroupUpdated

--- resources/lang/en/txtcmdr.php
+++ resources/lang/en/txtcmdr.php
@@ -2,9 +2,10 @@
 
 return [
 	'commander' => [
-		'optin' => "You have opted in using :mobile. \n-:signature",
-		'area'  => "You are now operating in :area. \n-:signature",
-		'tag'   => "You have a new keyword :code. \n-:signature",
+		'optin' 	  => "You have opted in using :mobile. \n-:signature",
+		'area'  	   => "You are now operating in :area. \n-:signature",
+		'group'  	   => "You joined :group. \n-:signature",
+		'tag'   	   => "You have a new keyword :code. \n-:signature",
 		'registration' => ":handle, you have registered. \n-:signature",
 	],
 ];
