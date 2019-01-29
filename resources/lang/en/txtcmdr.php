bca2086d2b16d2dff279edb6649e60acfd9fc08c
Lester Hurtado <lester@hurtado.ph>
2019-01-29 14:21:00 +0800

    add notification templates to CommanderAlertUpdated and CommanderAttributeUpdated

--- resources/lang/en/txtcmdr.php
+++ resources/lang/en/txtcmdr.php
@@ -7,5 +7,7 @@ return [
 		'group'  	   => "You joined :group. \n-:signature",
 		'tag'   	   => "You have a new keyword :code. \n-:signature",
 		'registration' => ":handle, you have registered. \n-:signature",
+		'alert'		   => "You sent a :alert alert. \n-:signature",
+		'attribute'	   => "You changed a :attribute attribute. \n-:signature",
 	],
 ];
