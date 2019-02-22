abba89dc1283b2b815cabe76bde2be476f64773c
Lester Hurtado <lester@hurtado.ph>
2019-02-22 08:33:11 +0800

    install and check out excel exports

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use Maatwebsite\Excel\Facades\Excel;
+use App\Campaign\Exports\ContactsExport;
+
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -7,3 +11,7 @@ Route::get('/', function () {
 Route::match(['get', 'post'], 'webhook/telerivet/airtime', 'TelerivetAirtimeController@handle');
 Route::match(['get', 'post'], 'webhook/engagespark/sms', 'EngageSparkSMSController@handle');
 Route::match(['get', 'post'], 'webhook/engagespark/airtime', 'EngageSparkAirtimeController@handle');
+
+Route::get('/exports/contacts', function() {
+    return Excel::download(new ContactsExport, 'contacts.xlsx');
+});
