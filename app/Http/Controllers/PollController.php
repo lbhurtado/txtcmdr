8a3d4518ff89e2b0f4c3f7dee30c1c837b81825d
Lester Hurtado <lester@hurtado.ph>
2019-03-03 23:23:47 +0800

    add poll views

---
+++ app/Http/Controllers/PollController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Campaign\Domain\Classes\Poll;
+use Illuminate\Support\Facades\Response;
+
+class PollController extends Controller
+{
+    public function poll_town()
+    {
+        $report = Poll::report_town()->groupBy(['town', 'position']);
+
+        return Response::view('txtcmdr.poll.test', ['areas' => $report])->header('Content-Type', 'text/plain');
+    }
+
+    public function poll_district()
+    {
+        $report = Poll::report_district()->groupBy(['district', 'position']);
+
+        return Response::view('txtcmdr.poll.test', ['areas' => $report])->header('Content-Type', 'text/plain');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -65,40 +65,6 @@ Route::get('/poll4', function() {
             ->get();
 });
 
-Route::get('/poll5', function() {
-        return DB::table('area_issue')
-            ->selectRaw("concat_ws(', ', `towns`.`name`, `districts`.`name`) as town")
-            ->addSelect('categories.name as position')
-            ->addSelect('issues.name as candidate')
-            ->selectRaw('sum(`area_issue`.`qty`) as `votes`')
-            ->join('issues', 'issues.id', '=', 'area_issue.issue_id')
-            ->join('categories', 'categories.id', '=', 'issues.category_id')
-            ->join('areas as precincts', 'precincts.id', '=', 'area_issue.area_id')
-            ->join('areas as barangays', 'barangays.id', '=', 'precincts.parent_id')
-            ->join('areas as towns', 'towns.id', '=', 'barangays.parent_id')
-            ->join('areas as districts', 'districts.id', '=', 'towns.parent_id')
-            ->whereIn('categories.id', [1, 2, 3])
-            ->groupBy('town', 'position', 'candidate')
-            ->orderByRaw('`town`, `categories`.`id`, `votes` desc')
-            ->get();
-});
+Route::get('/report/town', 'PollController@poll_town');
 
-Route::get('/poll6', function() {
-    return
-        DB::table('area_issue')
-            ->selectRaw("concat_ws(', ', `districts`.`name`) as district")
-            ->addSelect('categories.name as position')
-            ->addSelect('issues.name as candidate')
-            ->selectRaw('sum(`area_issue`.`qty`) as `votes`')
-            ->join('issues', 'issues.id', '=', 'area_issue.issue_id')
-            ->join('categories', 'categories.id', '=', 'issues.category_id')
-            ->join('areas as precincts', 'precincts.id', '=', 'area_issue.area_id')
-            ->join('areas as barangays', 'barangays.id', '=', 'precincts.parent_id')
-            ->join('areas as towns', 'towns.id', '=', 'barangays.parent_id')
-            ->join('areas as districts', 'districts.id', '=', 'towns.parent_id')
-            ->whereIn('categories.id', [1])
-            ->groupBy('district', 'position', 'candidate')
-            ->orderByRaw('`district`, `categories`.`id`, `votes` desc')
-            ->get();
-});
-
+Route::get('/report/district', 'PollController@poll_district');
